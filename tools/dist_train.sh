cd20d7b1a2e5ced87d3fd54a16281dd11c441820
bilallbayrakdar <bilal.bayrakdar2017@gtu.edu.tr>
2022-09-23 17:53:05 +0300

    mmocr forked

---
+++ tools/dist_train.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+if [ $# -lt 3 ]
+then
+    echo "Usage: bash $0 CONFIG CHECKPOINT GPUS"
+    exit
+fi
+
+CONFIG=$1
+WORK_DIR=$2
+GPUS=$3
+CHECKPOINT=$4
+NNODES=${NNODES:-1}
+NODE_RANK=${NODE_RANK:-0}
+PORT=${PORT:-29500}
+MASTER_ADDR=${MASTER_ADDR:-"127.0.0.1"}
+
+if [ ${GPUS} == 1 ]; then
+    python $(dirname "$0")/train.py  $CONFIG --work-dir=${WORK_DIR} --resume-from=${CHECKPOINT} ${@:5}
+else
+    python -m torch.distributed.launch \
+        --nnodes=$NNODES \
+        --node_rank=$NODE_RANK \
+        --master_addr=$MASTER_ADDR \
+        --nproc_per_node=$GPUS \
+        --master_port=$PORT \
+        $(dirname "$0")/train.py \
+        $CONFIG \
+        --seed 0 \
+        --work-dir=${WORK_DIR} \
+        --launcher pytorch ${@:4}
+fi
